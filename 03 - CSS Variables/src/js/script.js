5cd12d6f4546008053f2fa9cf5ea47aad8db1482
Brandon Leichty <brandonleichty@Brandons-MBP.domain>
2017-12-17 11:48:19 -0600

    JS adds event listener to all control inputs and logs value to console

--- 03 - CSS Variables/src/js/script.js
+++ 03 - CSS Variables/src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const inputs = document.querySelectorAll('.controls input');
+
+//logs the value passed to function
+function handleUpdate() {
+    console.log(this.value);
+}
+
+//calls 'handleUpdate' function when any change is made to input value
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+
+//calls 'handleUpdate' function when any change is made to mousemove value
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
